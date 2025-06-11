9a791f1fca3d6beef57c29b2192538f4c33a271a
tempchuks <tempchuks60@gmail.com>
2025-06-11 00:03:34 -0700

    added lesson1 and 2

---
+++ lesson-02/variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a = 3;
+    float b = 3.5;
+    double c = 4.5;
+    float sum = a+b+c;
+
+    printf("The total sum of the numbers is: %f\n",sum);
+    return 0;
+}
